7408fe9fe1568113e9d3f7f34df7449b618af636
Rinmolu <rinmoluadewumi@gmail.com>
2025-07-07 19:43:13 +0100

    Commit

---
+++ Activity1.py
@@ -0,0 +1,7 @@
+from datetime import datetime, date, time
+
+today= date.today()
+now= datetime.now()
+print("Todays Date Is " ,today)
+print("The Current Date And Time Is " ,now)
+print("Date Components " ,today.year, today.month, today.day)
